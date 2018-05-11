2a70c5d964d3f4e876a5402f659930daae874205
Bailin Huang <bailin.huang@ucr.ac.cr>
2018-05-11 11:24:54 -0600

    adds Modules, Query, Generator classes

--- src/Simulator.java
+++ src/Simulator.java
@@ -1,2 +1,17 @@
 public class Simulator {
+    private Interface anInterface;
+    private RandomValueGenerator valueGenerator;
+
+    public Simulator(Interface anInterface){
+        valueGenerator = new RandomValueGenerator();
+        this.anInterface = anInterface;
+    }
+
+    public void run(){
+
+    }
+    
+    private void generateQuery(){
+
+    }
 }
