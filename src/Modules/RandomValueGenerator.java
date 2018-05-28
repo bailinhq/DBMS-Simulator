9010638d4b3caeefa12290aa822ec34e67d08987
Gerald Vindas <gerald.vindas@ucr.ac.cr>
2018-05-27 19:10:33 -0600

    Se agrega clase de valores aleatorios

--- src/Modules/RandomValueGenerator.java
+++ src/Modules/RandomValueGenerator.java
@@ -1,15 +1,32 @@
 package Modules;
 
 public class RandomValueGenerator {
-    public RandomValueGenerator(){
+    RandomValueGenerator(){
 
     }
 
-    public double generateNormalDistributionValue(){
-        return 0.0;
+    public double generateNormalDistributionValue( double normalMean, double variance ){
+        //Metodo de convolucion
+        double sum = 0;
+        for (int i = 0; i < 12; i++) {
+            sum += Math.random();
+        }
+        sum -= 6;
+
+        return normalMean + (Math.sqrt(variance) * sum);
     }
 
-    public double generateExponentialDistributionValue(){
-        return 0.0;
+    public double generateExponentialDistributionValue(double lambda){
+
+        return -(1/lambda)*Math.log10(Math.random());
     }
+
+    public double generateUniformDistributionValue(double a, double b){
+        return a + ((b - a) * Math.random());
+    }
+
+    public double generateDiscreteValues(){
+        return Math.random();
+    }
+
 }
