f9393a70fc993fe14b2363165197241490ef95b4
Gerald Vindas <gerald.vindas@ucr.ac.cr>
2018-05-30 13:34:40 -0600

    Se agrega clase de estadistica de consultas

--- src/Statistics/QueryStatistics.java
+++ src/Statistics/QueryStatistics.java
@@ -1,4 +1,43 @@
 package Statistics;
 
 public class QueryStatistics {
+    private double arrivalTime;
+    private double departureTime;
+    private double timeOfQuery;
+
+    public QueryStatistics(double time){
+        arrivalTime = time;
+        departureTime = 0;
+        timeOfQuery = 0;
+    }
+
+    public double getTimeInSystem(){
+        return departureTime-arrivalTime;
+    }
+
+    public double getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(double arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public double getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(double departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public double getTimeOfQuery() {
+        return timeOfQuery;
+    }
+
+    public void setTimeOfQuery(double timeOfQuery) {
+        this.timeOfQuery = timeOfQuery;
+    }
+
+
 }
