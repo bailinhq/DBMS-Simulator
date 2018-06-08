001307da25c1e13058de9cbc87a1231b252e6466
Bailin Huang <bailin.huang@ucr.ac.cr>
2018-06-08 09:52:38 -0600

    added generateQuery and changed EventType + constructed Event

--- src/Modules/Simulator.java
+++ src/Modules/Simulator.java
@@ -3,6 +3,7 @@ package Modules;
 import Modules.*;
 
 import java.util.PriorityQueue;
+import java.util.Random;
 
 public class Simulator {
     private RandomValueGenerator valueGenerator;
@@ -18,7 +19,10 @@ public class Simulator {
     private double timeout;
     private int p;
 
+    private Random randomGenerator;
+
     public Simulator(){
+        randomGenerator = new Random(0);
         valueGenerator = new RandomValueGenerator();
     }
 
@@ -27,9 +31,25 @@ public class Simulator {
     }
 
     private Query generateQuery(){
-        Query query = new Query(QueryType.DDL);
+        double random = randomGenerator.nextDouble();
+        Query query;
+        if (random <= 0.30){
+            query = new Query(QueryType.SELECT);
+        } else if (random <= 0.55){
+            query = new Query(QueryType.UPDATE);
+        } else if (random <= 0.90){
+            query = new Query(QueryType.JOIN);
+        } else {
+            query = new Query(QueryType.DDL);
+        }
         return query;
     }
+
+//    private Event generateEvent(){
+//        Query query = new Query();
+//        Event event = new Event(query, EventType.ARRIVAL);
+//        return event;
+//    }
 
     private void simulate(){
         
