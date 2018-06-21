c4f8c8c135a23852165f76a0feaf38b62ca52247
Bailin Huang <bailin.huang@ucr.ac.cr>
2018-06-21 10:47:51 -0600

    query javadoc

--- src/main/java/Event/Query.java
+++ src/main/java/Event/Query.java
@@ -14,8 +14,10 @@ public class Query {
     private int numberOfBlocks;
     private QueryStatistics queryStatistics;
 
-
-
+    /**
+     * Initializes a Query with a QuerType.
+     * @param queryType
+     */
     public Query(QueryType queryType){
         type = queryType;
         setNumbersOfBlocks();
@@ -23,6 +25,9 @@ public class Query {
         queryStatistics = new QueryStatistics();
     }
 
+    /**
+     * Sets the number of blocks for each query to be used in the simulation.
+     */
     private void setNumbersOfBlocks(){
         switch (type){
             case JOIN:
@@ -40,6 +45,9 @@ public class Query {
         }
     }
 
+    /**
+     * Sets the time optimization for each type of query.
+     */
     private void setTimeOptimization(){
         switch (type){
             //Read Only
@@ -60,7 +68,10 @@ public class Query {
         }
     }
 
-
+    /**
+     * Returns the priority depending of the QueryType.
+     * @return priority.
+     */
     public int getPriority(){
         //top priority 1
         int priority = 0;
@@ -80,18 +91,34 @@ public class Query {
         return priority;
     }
 
+    /**
+     * Returns the Time Optimization.
+     * @return Time Optimization.
+     */
     public double getTimeOptimization() {
         return timeOptimization;
     }
 
+    /**
+     * Returns the number of blocks.
+     * @return number of blocks.
+     */
     public int getNumberOfBlocks() {
         return numberOfBlocks;
     }
 
+    /**
+     * Returns the type of query.
+     * @return type of query.
+     */
     public QueryType getType() {
         return type;
     }
 
+    /**
+     * Returns the QueryStatistics.
+     * @return QueryStatistics
+     */
     public QueryStatistics getQueryStatistics() {
         return queryStatistics;
     }
